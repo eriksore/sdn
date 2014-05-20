ea611dbec5abca86e8602885f4c3962f8bd79095
Erik Sørensen <esorensen.private@gmail.com>
2014-05-20 21:48:24 +0200

    update

--- test2.py
+++ test2.py
@@ -15,48 +15,29 @@ def federatedNet():
 
 	print "*** Creating Nodes ***"
 	print "*** Adding remote controller ***"
-	c0 = net.addController( 'c0', ip='127.0.0.1', port=6633)
+	c0 = net.addController( 'c0', ip='192.168.231.246', port=6633)
 	
 	print "*** Adding switches ***"
-	switches = [ net.addSwitch( 's%s' % s ) for s in irange( 1, 6 ) ]
+	switches = [ net.addSwitch( 's%s' % s ) for s in irange( 1, 5 ) ]
 	print switches
 	print "*** Adding hosts ***"
 	hosts = {}
-	for h in irange( 1, 4):
+	for h in irange( 1, 2):
 		globals()['h'+str(h)] = net.addHost( 'h%s' % h, mac='00:00:00:00:00:0%s' % h, ip='10.0.0.%s/8' % h )
-	for h in irange( 5, 8):
-		globals()['h'+str(h)] = net.addHost( 'h%s' % h, mac='00:00:00:00:00:0%s' % h, ip='11.0.0.%s/8' % h )
-	
-	linkoptscore = dict(bw=1000, delay='0ms', loss=0)
-	linkoptsaccess = dict(bw=100, delay='0ms', loss=0)
-	linkoptshost = dict(bw=10, delay='0ms', loss=0)
 	
 	print "*** Add links between switches ***"
-	net.addLink(switches[0],switches[1], **linkoptsaccess)
-	net.addLink(switches[0],switches[2], **linkoptsaccess)
-	net.addLink(switches[0],switches[3], **linkoptscore)
-	net.addLink(switches[3],switches[4], **linkoptsaccess)
-	net.addLink(switches[3],switches[5], **linkoptsaccess)
-	
+	net.addLink(s1,s2)
+	net.addLink(s1,s3)
+	net.addLink(s2,s5)
+	net.addLink(s3,s4)
+	net.addLink(s4,s5)
 	print "*** Add links to hosts ***"
-	net.addLink(switches[1],h1,**linkoptshost)
-	net.addLink(switches[1],h2,**linkoptshost)
-	net.addLink(switches[2],h3,**linkoptshost)
-	net.addLink(switches[2],h4,**linkoptshost)
-	net.addLink(switches[4],h5,**linkoptshost)
-	net.addLink(switches[4],h6,**linkoptshost)
-	net.addLink(switches[5],h7,**linkoptshost)
-	net.addLink(switches[5],h8,**linkoptshost)
+	net.addLink(s1,h1)
+	net.addLink(s5,h2)
 
 	net.build()
 	net.start()	
-	print "*** Set all switches to fail-mode STANDALONE"
-	call(["sudo", "ovs-vsctl", "set-fail-mode", "s1", "standalone"])
-	call(["sudo", "ovs-vsctl", "set-fail-mode", "s2", "standalone"])
-	call(["sudo", "ovs-vsctl", "set-fail-mode", "s3", "standalone"])
-	call(["sudo", "ovs-vsctl", "set-fail-mode", "s4", "standalone"])
-	call(["sudo", "ovs-vsctl", "set-fail-mode", "s5", "standalone"])
-	call(["sudo", "ovs-vsctl", "set-fail-mode", "s6", "standalone"])
+	
 
 	CLI( net ) 
 	net.stop()
